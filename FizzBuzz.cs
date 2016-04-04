bd4e1a97e2a0e170a0e2b9d6876893fa2c2a99ba
YonM <yonathanmaalo@gmail.com>
2016-04-04 21:26:50 +0100

    First commit

---
+++ FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Fizzbuzz
+    {
+        static void Main(string[] args)
+        {
+            Fizzbuzz fizzbuzz = new Fizzbuzz();
+            for (int i = 1; i < 100; i++){
+                Console.WriteLine(fizzbuzz.fizzbuzzer(i));
+            }
+        }
+
+        public string fizzbuzzer(int v)
+        {
+            string result="";
+            if( v % 3 == 0)
+            {
+                result = "fizz";
+            }
+            if (v % 5 == 0)
+            {
+                result += "buzz";
+            }
+
+            result = String.IsNullOrEmpty(result) ?  v.ToString(CultureInfo.InvariantCulture) : result;
+            return result;
+
+        }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public sealed class FizzBuzzTest
+    {
+        private static Fizzbuzz fizzbuzz;
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            fizzbuzz = new Fizzbuzz();
+        }
+        [TestMethod]
+        public void printIntegerFor1()
+        {
+            Assert.AreEqual("1", fizzbuzz.fizzbuzzer(1)); 
+        }
+
+        [TestMethod]
+        public void printIntegerFor2()
+        {
+            Assert.AreEqual("2", fizzbuzz.fizzbuzzer(2));
+        }
+
+        [TestMethod]
+        public void printFizzFor3()
+        {
+            Assert.AreEqual("fizz", fizzbuzz.fizzbuzzer(3));
+        }
+
+        [TestMethod]
+        public void printFizzFor9()
+        {
+            Assert.AreEqual("fizz", fizzbuzz.fizzbuzzer(9));
+        }
+
+        [TestMethod]
+        public void printBuzzFor5()
+        {
+            Assert.AreEqual("buzz", fizzbuzz.fizzbuzzer(5));
+        }
+
+        [TestMethod]
+        public void printBuzzFor20()
+        {
+            Assert.AreEqual("buzz", fizzbuzz.fizzbuzzer(20));
+        }
+
+        [TestMethod]
+        public void printFizzBuzz15()
+        {
+            Assert.AreEqual("fizzbuzz", fizzbuzz.fizzbuzzer(15));
+        }
+
+        [TestMethod]
+        public void printFizzBuzz45()
+        {
+            Assert.AreEqual("fizzbuzz", fizzbuzz.fizzbuzzer(45));
+        }
+    }
+}
